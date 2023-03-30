969ce455d3c83a068dc48aa9103a3ac9574f6db4
Getoar Arifaj <ga47117@ubt-uni.net>
2023-03-30 02:56:56 +0200

    Added the room model

---
+++ API/Models/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomNumber { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
