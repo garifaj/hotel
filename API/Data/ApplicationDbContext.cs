2e4d8290edabdb75fd40b0d49cf7387bb39d8ea5
Getoar Arifaj <ga47117@ubt-uni.net>
2023-03-30 03:15:51 +0200

    Created the first db table using migrations

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
